24ce22d60401f248936e933042c83c9fa1f3b84a
Jared Weiss <jbw825@JW-Mac-2.local>
2014-01-07 00:12:11 -0500

    FUCK AUBURN!

---
+++ QuickBill/Barcode.h
@@ -0,0 +1,13 @@
+//
+//  Barcode.h
+//  QuickBill
+//
+//  Created by Jared Weiss on 1/7/14.
+//  Copyright (c) 2014 Mutton Button. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Barcode : NSObject
+
+@end
---
+++ QuickBill/Barcode.m
@@ -0,0 +1,13 @@
+//
+//  Barcode.m
+//  QuickBill
+//
+//  Created by Jared Weiss on 1/7/14.
+//  Copyright (c) 2014 Mutton Button. All rights reserved.
+//
+
+#import "Barcode.h"
+
+@implementation Barcode
+
+@end
