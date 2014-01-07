ddd2b4c40382418dac6250d6f1a9033daf7a9e4f
Jared Weiss <jbw825@JW-Mac-2.local>
2014-01-06 22:01:13 -0500

    Initial commit from local git

---
+++ QuickBill/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QuickBill
+//
+//  Created by Jared Weiss on 1/6/14.
+//  Copyright (c) 2014 Mutton Button. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ QuickBillTests/QuickBillTests.m
@@ -0,0 +1,34 @@
+//
+//  QuickBillTests.m
+//  QuickBillTests
+//
+//  Created by Jared Weiss on 1/6/14.
+//  Copyright (c) 2014 Mutton Button. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface QuickBillTests : XCTestCase
+
+@end
+
+@implementation QuickBillTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
